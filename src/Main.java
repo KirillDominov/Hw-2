6dae94493a550959d8cb36833b0dd971fa442e82
КириллДоминов <spotik321@gmail.com>
2024-08-24 23:29:17 +0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       var dog = 8.0;
+       var cat = 3.6;
+       var paper = 763789;
+        System.out.println(dog);
+        System.out.println (cat);
+        System.out.println (paper);
+        System.out.println();
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println (cat);
+        System.out.println (paper);
+        System.out.println();
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println (cat);
+        System.out.println (paper);
+        System.out.println();
+        var friend = 19;
+        System.out.println (friend);
+        friend = friend + 2;
+        System.out.println (friend);
+        friend = friend / 7;
+        System.out.println (friend);
+        System.out.println();
+        var frog = 3.5;
+        System.out.println (frog);
+        frog = frog * 10;
+        System.out.println (frog);
+        frog = frog / 3.5;
+        System.out.println (frog);
+        frog = frog +4;
+        System.out.println (frog);
+        System.out.println();
+        var weightBoxer1 = 78.2;
+        var weightBoxer2 = 82.7;
+        var weightAllBoxer = weightBoxer1 + weightBoxer2;
+        System.out.println (weightAllBoxer);
+        var differenceWeightBoxer = weightBoxer2 - weightBoxer1;
+        System.out.println (differenceWeightBoxer);
+        var difference = weightBoxer2 % weightBoxer1;
+        System.out.println (difference);
+        System.out.println();
+        var watch = 640;
+        var watchJob = 8;
+        var people = watch / watchJob;
+        System.out.println ("Всего работников в компании — " + people + " человек.");
+        people = people+94;
+        watch = watchJob * people;
+        System.out.println ("Если в компании работает "+ people + " человек, то всего " + watch  + " часов работы может быть поделено между сотрудниками");
+
+
+
+
+
+
+
     }
 }
